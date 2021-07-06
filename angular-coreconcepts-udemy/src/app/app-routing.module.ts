08c51df0df9970ca374690868fbadb5b55de8534
Pravinmadurai <pravinkumarmg1996@gmail.com>
2021-07-06 18:15:46 +0530

    Update Commit

--- angular-coreconcepts-udemy/src/app/app-routing.module.ts
+++ angular-coreconcepts-udemy/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseCardComponent } from './course-card/course-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'Courses' , component: CourseCardComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-coreconcepts-udemy/src/app/course-card/course-card.component.ts
+++ angular-coreconcepts-udemy/src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-card.component.scss']
 })
 export class CourseCardComponent implements OnInit {
-
+  @Input() title: string | undefined;
   constructor() { }
 
   ngOnInit(): void {
